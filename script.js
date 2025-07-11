45fdefc204606d6ea3736cc912388846a4979a19
Camila Rocha <63120151+CamilaSRocha@users.noreply.github.com>
2025-07-11 17:01:09 -0300

    correção de um erro

--- script.js
+++ script.js
@@ -10,7 +10,6 @@ const markdownToHTML = (text) => {
     return converter.makeHtml(text)
 }
 
-// AIzaSyAq6rMAVxBut34DNJDqHPCA3k0Sf5ATD84
 const perguntarAI = async (question, game, apiKey)  => {
     const model = "gemini-2.5-flash"
     const geminiURL = `https://generativelanguage.googleapis.com/v1beta/models/${model}:generateContent?key=${apiKey}`
