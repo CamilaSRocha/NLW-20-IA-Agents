1eb582383c69db54db450b973715d5ec0fc06158
Camila Rocha <63120151+CamilaSRocha@users.noreply.github.com>
2025-07-10 21:11:23 -0300

    finalizando o projeto adicionando a ia gemini do google, colocando prompts de comando para deixar a resposta mais assertiva e dandos os retoques finais na estética do projeto

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 # HTML
-
 - HypertText
     - Textos puros, livros, revistas, jornais;
     - Links: acessar outros documentos;
@@ -12,6 +11,7 @@
 - Language
     - Linguagem
     - Sintaxe: maneira de escrever
+
 # HTTP
 - HyperText
 - Transfer
@@ -19,6 +19,9 @@
 - Protocol
     - Protocolo
     - Conjunto de regras
+- Methods HTTP (verbos): Get, Post, Patch/Put, Delete
+- Headers (cabeçalhos): Intruções/Informações extras para cada chamada
+
 # URL
 - Uniform
     - Uniforme
@@ -26,11 +29,13 @@
     - Recurso
 - Locator
     - Localizador Encontrar um recurso (html, css, js, pdf, jpg, mp4, mpr, ...) Endereço
+
 # IP
 - Internet
     - Rede mundial de computadores
 - Protocol
     - Conjunto de regras Endereço do computador rocketseat.com.br (domínio) 123.32.1.23 (ip)
+
 # DNS
 - Domain
     - Domínio
@@ -38,6 +43,7 @@
     - Nome
 - Server
     - Servidor
+
 # CSS
 - Cascading
     - Cascata
@@ -83,3 +89,29 @@ De um arquivo que terá na sua sintaxe, declarações, propriedades e valores. A
 - Document
 - Object
 - Model
+
+# API
+- Application
+- Programming
+- Interface
+- JSON: JavaScript Object Notation
+
+# CDN
+- Content 
+- Delivery
+- Network
+
+# LLM 
+- Large
+- Language
+- Model
+- Gemini, OpenAI (chatGPT), Anthropic (Claude)...
+
+# Agentes AI
+- Tools (ferramentas): permitem uso de código ou apps ou qualquer informação extra
+- Melhor contexto
+
+# Engenharia de prompt
+- One shot: uma única pergunta sem muito contexto
+- Few shot: é apresentado exemplos do que se espera
+- Chain of Thought: cadeia de pensamento para a IA responder gradativamente conforme a instrução
--- script.js
+++ script.js
@@ -5,17 +5,75 @@ const askButton = document.getElementById('askButton')
 const aiResponse = document.getElementById('aiResponse')
 const form = document.getElementById('form')
 
-const perguntarAI = async () => {
-    
+const markdownToHTML = (text) => {
+    const converter = new showdown.Converter()
+    return converter.makeHtml(text)
 }
 
-const enviarFormulario = (event) => {
+// AIzaSyAq6rMAVxBut34DNJDqHPCA3k0Sf5ATD84
+const perguntarAI = async (question, game, apiKey)  => {
+    const model = "gemini-2.5-flash"
+    const geminiURL = `https://generativelanguage.googleapis.com/v1beta/models/${model}:generateContent?key=${apiKey}`
+    const pergunta = `
+        ## Especialidade
+        Você é um especialista assistente de meta para o jogo ${game}
+        
+        ## Tarefa
+        Você deve responder as perguntas do usuário com base no seu conhecimento do jogo, estratégias, build e dicas
+        
+        ## Regras
+        - Se você não sabe a resposta, responda com 'Não sei' e não tente inventar uma resposta.
+        - Se a pergunta não está relacionada ao jogo, responda com 'Essa pergunta não está relacionada com o jogo'.
+        - Considere a data atual ${new Date().toLocaleDateString()}
+        - Faça pesquisas atualizadas sobre o patch atual, baseado na data atual, para dar uma resposta coerente.
+        - Nunca responda itens que você não tenha certeza de que existe no patch atual.
+        
+        ## Resposta
+        - Economize na resposta, seja direto e responda no máximo 500 caracteres. 
+        - Responda em markdown.
+        - Não precisa fazer nenhuma saudação ou despedida, apenas responda o que o usuário está querendo. 
+        
+        ## Exemplo de resposta
+        pergunta do usuário: melhor build rengar jungle
+        resposta: A build mais atual é: \n\n **Itens:** \n\n coloque os itens aqui \n\n **Runas:** \n\n exemplo de runas \n\n
+
+        ---
+        Aqui está a pergunta do usuário: ${question}
+
+    `  
+
+    const contents = [{
+        role: "user",
+        parts: [{
+            text: pergunta
+        }]
+    }]
+
+    const tools = [{
+        google_search: {}
+    }]
+
+    // chamada API
+    const response = await fetch(geminiURL, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        }, 
+        body: JSON.stringify({
+            contents,
+            tools
+        })
+    })
+
+    const data = await response.json()
+    return data.candidates[0].content.parts[0].text
+}
+
+const enviarFormulario = async (event) => {
     event.preventDefault()
     const apiKey = apiKeyInput.value
     const game = gameSelect.value
     const question = questionInput.value
-
-    console.log({apiKey, game, question})
 
     if(apiKey == '' || game == '' || question == '') {
         alert('Por favor, preencha todos os campos')
@@ -28,6 +86,10 @@ const enviarFormulario = (event) => {
 
     try {
         //perguntar para a IA
+        const text = await perguntarAI (question, game, apiKey)
+        aiResponse.querySelector('.response-content').innerHTML = markdownToHTML(text)
+        aiResponse.classList.remove('hidden')
+
     } catch(error) {
         console.log('Erro: ', error)
     } finally { 
